1c8971e3a1279f330f76503f77277cb5d9abf5b2
juancruzfernandez <jcfernandezok@gmail.com>
2023-03-21 16:30:34 -0400

    created a batchImport...

---
+++ server/batchImport.js
@@ -0,0 +1,25 @@
+const { MongoClient } = require("mongodb");
+const items = require("./data/items.json");
+const companies = require("./data/companies.json");
+
+require("dotenv").config();
+const { MONGO_URI } = process.env;
+const options = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+};
+
+
+const batchImport = async () => {
+  const client = new MongoClient(MONGO_URI, options);
+  await client.connect();
+  const db = client.db("eCommerce");
+
+  
+  await db.collection("items").insertMany(items);
+  await db.collection("companies").insertMany(companies);
+
+  client.close();
+};
+
+batchImport();
