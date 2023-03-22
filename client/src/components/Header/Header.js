1fc0d88f7c4e1cd4ef24ae19db0699cb249ab621
thalieskoda <thalieskoda@hotmail.com>
2023-03-22 13:55:35 -0400

    changes on the searchBar

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,6 +1,11 @@
+import SearchBar from "../SearchBar"
+
 const Header = () => {
 return(
+    <>
 <h1>This is our headerrrr</h1>
+<SearchBar/>
+    </>
     )
 
 }
--- client/src/components/SearchBar.js
+++ client/src/components/SearchBar.js
@@ -1,36 +1,27 @@
 import { useState } from "react"
+import {BsSearch} from "react-icons/bs"
 import styled from "styled-components";
-const items = require("./data/items.json");
-const companies = require("./data/companies.json");
 
-const mergedData = items.concat(companies);
 const SearchBar = () => {
 
 const [userInput, setUserInput]= useState("");
 const [suggestion, setSuggestion] = useState([]);
 
-
-const matchedSuggestions = mergedData.filter((data)=> {
-return(
-    data.name.toLowerCase().includes(userInput.toLowerCase()) && userInput.length>=1
-);
-
-});
 return(
 <Wrapper>
-<Container>
+<div>
         <Input
           type="text"
           value={userInput}
           onChange={(ev) => setUserInput(ev.target.value)}
           onKeyDown={(ev) => {
-            if (ev.key === "Enter") {
-              handleSelect(ev.target.useInput);
-            }
+            // if (ev.key === "Enter") {
+            //   handleSelect(ev.target.useInput);
+            // }
           }}
         />
-        <Button onClick={() => setUserInput("")}>Clear</Button>
-      </Container>
+        <Button onClick={() => setUserInput("")}><BsSearch/></Button>
+      </div>
 
 </Wrapper>
 )
@@ -39,29 +30,31 @@ return(
 const Wrapper = styled.div`
   display: flex;
   flex-direction: column;
-  align-items: center;
+
   margin: 20px;
 `;
 
 const Button = styled.button`
-  background-color: blue;
+  background-color: green;
   color: white;
   border-radius: 5px;
-  padding: 5px 20px;
+  padding: 5px 15px;
   margin: 0px 0px 0px 10px;
   border: 1px solid grey;
 
   &:focus {
-    border: 2px solid rgb(68, 140, 255);
+    border: 2px solid green;
   }
 `;
+
 const Input = styled.input`
-  width: 400px;
+  width: 250px;
   border-radius: 5px;
   border: 1px solid grey;
 
   &:focus {
     border: 4px solid rgb(68, 140, 255);
   }
+`
 
 export default SearchBar
