60ef52ff53f6aab6e4b67113ea2856009746b68c
thalieskoda <thalieskoda@hotmail.com>
2023-03-25 16:32:20 -0400

    searchpage

--- client/src/components/SearchBar.js
+++ client/src/components/SearchBar.js
@@ -110,7 +110,7 @@ const Results = styled.ul`
   box-shadow: rgba(99, 99, 99, 0.2) 0px 2px 8px 0px;
   z-index: 1;
   background-color:white;
-  opacity:90%
+  opacity:90%;
 `;
 
 const Container = styled.div`
--- client/src/components/SearchPage.js
+++ client/src/components/SearchPage.js
@@ -1,6 +1,177 @@
+import { useEffect, useState } from "react";
+import styled from "styled-components";
+import { FiLoader, FiSearch } from "react-icons/fi";
+import { useNavigate } from "react-router-dom";
+import { NavLink } from "react-router-dom";
+
 const SearchPage = () => {
+  const [userInput, setUserInput] = useState("");
+  const [itemSuggestion, setItemSuggestion] = useState([]);
 
+  const navigate = useNavigate();
 
-}
+  //Fetching all of the items.
+  useEffect(() => {
+    fetch("/getItems")
+      .then((res) => res.json())
+      .then((data) => {
+        setItemSuggestion(data.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, [userInput]);
 
-export default SearchPage
+  //if the itemsSuggestions === to the userInput. We only want the 10 first suggestions.
+  const matchedSuggestions =
+    itemSuggestion &&
+    itemSuggestion
+      .filter((suggestion) => {
+        return (
+          suggestion.name.toLowerCase().includes(userInput.toLowerCase()) &&
+          userInput.length >= 2
+        );
+      })
+
+  //Navigate to the selected item in the suggestion according to its _id
+  // set the input to ""
+  // Remove the itemsSuggestion since there's no more input.
+  const navigateSuggestions = (_id) => {
+    navigate(`/items/${_id}`);
+    setItemSuggestion([]);
+    setUserInput("");
+  };
+
+  console.log(itemSuggestion._id);
+  return (
+    <Wrapper>
+      <H1>What are you looking for ?</H1>
+      <Container>
+        <Input
+          type="text"
+          value={userInput}
+          onChange={(ev) => setUserInput(ev.target.value)}
+        />
+        <Button>
+          <FiSearch />
+        </Button>
+      </Container>
+
+      <Results>
+        {!itemSuggestion ? (
+          <LoadingIcon>
+            <FiLoader />
+          </LoadingIcon>
+        ) : (
+          matchedSuggestions.map((suggestion) => {
+            const indexTitle = suggestion.name
+              .toLowerCase()
+              .indexOf(userInput.toLowerCase());
+            const firstHalf = suggestion.name.slice(
+              0,
+              indexTitle + userInput.length
+            );
+            const secondHalf = suggestion.name.slice(
+              indexTitle + userInput.length
+            );
+
+            return (
+              <Suggestion
+                key={suggestion._id}
+                onClick={() => navigateSuggestions(matchedSuggestions[0]._id)}
+              >
+                <span>
+                  {firstHalf}
+                  <Prediction>{secondHalf}</Prediction>
+                </span>{" "}
+              </Suggestion>
+            );
+          })
+        )}
+      </Results>
+    </Wrapper>
+  );
+};
+const H1 = styled.h1`
+  text-align: center;
+  padding: 20px;
+`;
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  position: relative;
+  margin: 20px;
+  height: 80vh;
+`;
+
+const Button = styled.span`
+  color: black;
+  border-radius: 5px;
+  padding: 5px;
+  margin: 0px 0px 0px 10px;
+`;
+
+const Results = styled.ul`
+   position: absolute;
+  top: 20%; 
+  left: 30%; 
+  box-shadow: rgba(99, 99, 99, 0.2) 0px 2px 8px 0px;
+  z-index: 1;
+  background-color: white;
+  opacity: 90%;
+  width: 730px;
+  overflow-y: scroll; /* add this line */
+  max-height: 60vh; /* add this line */
+`;
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  padding: 10px;
+`;
+const Suggestion = styled.li`
+  padding: 6px;
+  list-style-type: none;
+  margin: 15px;
+
+  &:hover {
+    cursor: pointer;
+    background-color: #d4ff8a;
+    border-radius: 10px;
+  }
+`;
+
+const Input = styled.input`
+  width: 500px;
+  border-radius: 5px;
+  border: 1px solid grey;
+  height: 40px;
+  font-size: 20px;
+
+  &:focus {
+    border: 4px solid rgb(68, 140, 255);
+  }
+`;
+
+const Prediction = styled.span`
+  font-weight: bold;
+  color: black;
+`;
+
+const LoadingIcon = styled(FiLoader)`
+  position: relative;
+  left: 50%;
+  top: 10px;
+  animation: spin 1s infinite linear;
+  height: 80vh;
+
+  @keyframes spin {
+    100% {
+      transform: rotate(360deg);
+    }
+  }
+`;
+
+export default SearchPage;
